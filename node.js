997215a41060575dc3fb2acedf1202ddda3cd698
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2023-07-11 20:30:46 -0700

    Class 2 Assignment

---
+++ node.js
@@ -0,0 +1,56 @@
+
+//2nd requirement in the lab
+let username=prompt("What is your name?");
+alert(`Welcome ${username}.`)
+
+
+
+//1st requirement in the lab
+let name = prompt("Do I come from Seattle?");
+if (name.toLowerCase() == "yes"){
+   // console.log("Correct");
+   alert("Correct");
+} else {
+   // console.log("You are wrong.")
+   alert("Wrong answer");
+}
+
+
+let age = prompt("Am I 31 years old?");
+if (age.toLowerCase()=="yes"){
+   // console.log("Correct");
+   alert("Correct");
+} else {
+    //console.log("You are wrong");
+    alert("Wrong answer");
+}
+
+
+let food = prompt("Is my favorite food Ramen?");
+if (food.toLowerCase()=="yes"){
+   // console.log("Correct");
+   alert("Correct");
+} else {
+   // console.log("Wrong");
+   alert("Wrong answer");
+}
+
+
+let job = prompt("Was I ever a fisherman?");
+if (job.toLowerCase()=="yes"){
+   // console.log("Correct");
+   alert("Correct");
+} else {
+   // console.log("Wrong")
+   alert("Wrong answer");
+}
+
+
+let school = prompt("Did I go to school?");
+if (school.toLowerCase()=="yes"){
+   // console.log("Correct");
+   alert("Correct");
+} else {
+   // ("Wrong")
+   alert("Wrong answer");
+}
